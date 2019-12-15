f9fbcc9ce5b6582f363d21e963055bb2ea9a64d6
Takayuki Iida <allthamoon@yahoo.co.jp>
2019-12-16 03:35:18 +0900

    ss

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  CONNECTION_URL: process.env.MONGODB_CONNECTION_URL || "mongodb://user:user@localhost:27017/weblog",
-  DATABSE: process.env.MONGODB_DATABSE || "weblog",
+  CONNECTION_URL: process.env.MONGODB_CONNECTION_URL || "mongodb://gotz815:kingdom1413@cluster0-dtgdc.mongodb.net/test",
+  DATABSE: process.env.MONGODB_DATABSE || "test",
   OPTIONS: {
     family: 4
   }
